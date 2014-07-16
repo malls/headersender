4c808f6bbc8b81d17cce4eeea5e6ab6c20d1c8ba
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-16 17:49:37 -0400

    remove routes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var app = express();
 
 dotenv.load();
 
-
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
