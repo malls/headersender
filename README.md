913617e6b41ed9458b27c9485857369589ebaa8f
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-16 17:48:04 -0400

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+cute lil server that sends back the headers for any request made to it
+
+to set up: 
+- npm install 
+- npm start
--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var app = express();
 
 dotenv.load();
 
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send('gimme the root, gimme the root!');
+});
+
+app.get('/:anythingelse', function(req, res){
+  res.send(req.headers);
 });
 
 app.listen(process.env.PORT);
