4c51e7cfd1dd85867427321d8c636caca861fe90
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-16 18:00:43 -0400

    catchall

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ var app = express();
 
 dotenv.load();
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization, Accept-Site");
   next();
  });
 
-app.get('/', function(req, res){
-  res.send('gimme the root, gimme the root!');
-});
-
-app.get('/:anythingelse', function(req, res){
+app.get('*', function(req, res){
   res.send(req.headers);
 });
 
