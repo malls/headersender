d371ee48066eee3cbfc677aaf345d92ff29ac547
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-17 13:36:34 -0400

    change mock post structure

--- mocks/posts.js
+++ mocks/posts.js
@@ -1,9 +1,11 @@
 var posts = {
   post1: {
+    id: 1,
     title: "airplane",
     content: "MALAYSIA PASSENGER PLANE CRASHES IN EAST UKRAINE — 295 ABOARD — UKRAINIAN GOVERNMENT SAYS PLANE SHOT DOWN BY REBELS"
   },
   post2: {
+    id: 2,
   	title: "microsoft",
   	content: "Here's How Microsoft Exec Stephen Elop Told 12,500 Employees They Were Getting Laid Off"
   }
