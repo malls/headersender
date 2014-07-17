4945deaa44549ad1f9c23e84f40229d85ce3f6d0
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-17 16:18:20 -0400

    redundancy removal in user mock

--- mocks/users.js
+++ mocks/users.js
@@ -4,13 +4,11 @@ var users = [
   {
     name: "bob",
     age: 25,
-    name: "bob",
     password: "password123"
   },
   {
     name: "alice",
   	age: 23,
-  	name: "alice",
   	password: "password"
   }
 ];
