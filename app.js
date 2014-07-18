f5b33f4588e3170870f6dc8b7ab557fad4bfb9cc
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-18 15:52:06 -0400

    don't serve headers with every request

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
 var dotenv = require('dotenv'),
-	express = require('express'),
-	bodyParser = require('body-parser'),
-	db = require('./db/db'),
-	lib = require('./lib/library'),
-	app = express();
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    db = require('./db/db'),
+    lib = require('./lib/library'),
+    app = express();
 
 dotenv.load();
 
 app
-	.use(function (req, res, next) {
-	  res.header('Access-Control-Allow-Origin', '*');
-	  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, Accept-Site');
-	  next();
-	 })
-	.use(bodyParser.json ());
+    .use(function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, Accept-Site, Content-Type');
+      next();
+     })
+    .use(bodyParser.json ());
 
 app
-	.get('/:type/:id', function (req, res) {
-		var response = {};
-		var type = req.params.type;
-		response[type] = db[req.params.type].find(req.params.id);
-		response.headers = req.headers;
-	  	res.send(response);
-	})
-	.get('/:type', function (req, res) {
-		var response = {};
-		var type = req.params.type;
-		response[type] = db[type][type]; 
-		response.headers = req.headers;
-		res.send(response);
-	})
-	.post('/:type/:id', function (req, res) {
-		var response = {};
-		var type = req.params.type;
-		var id = req.params.id;
-		var list = db[type][type];
-		var index = db[type].index(id);
-		list[index] = lib.updater(list[index], req.body);
-		response[type] = db[type].find(id);
-		response.headers = req.headers;
-		res.send(response);
-	})
-	.post('/:type', function (req, res) {
-		var response = {};
-		var type = req.params.type;
-		var list = db[type][type];
-		list.push(req.body);
-		response[type] = list;
-		response.headers = req.headers;
-		res.send(response);
-	})
-	.get('*', function (req, res) {
-	  res.send(req.headers);
-	})
-	.post('*', function (req, res) {
-	  res.send(req.headers);
-	});
+    .get('/:type/:id', function (req, res) {
+        var response = {};
+        var type = req.params.type;
+        response = db[req.params.type].find(req.params.id);
+        //response.headers = req.headers;
+          res.send(response);
+    })
+    .get('/:type', function (req, res) {
+        var response = {};
+        var type = req.params.type;
+        response = db[type][type];
+        //response.headers = req.headers;
+        res.send(response);
+    })
+    .post('/:type/:id', function (req, res) {
+        var response = {};
+        var type = req.params.type;
+        var id = req.params.id;
+        var list = db[type][type];
+        var index = db[type].index(id);
+        list[index] = lib.updater(list[index], req.body);
+        response = db[type].find(id);
+        //response.headers = req.headers;
+        res.send(response);
+    })
+    .post('/:type', function (req, res) {
+        var response = {};
+        var type = req.params.type;
+        var list = db[type][type];
+        list.push(req.body);
+        response = list;
+        //response.headers = req.headers;
+        res.send(response);
+    })
+    .get('*', function (req, res) {
+      res.send(req.headers);
+    })
+    .post('*', function (req, res) {
+      res.send(req.headers);
+    });
 
 app.listen (process.env.PORT || 3000);
