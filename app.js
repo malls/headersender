f4d899d8fbc8ee77db6589835db045bac8bdf95a
Forrest Almasi <forrest.almasi@gmail.com>
2014-10-01 11:49:30 -0400

    more headers

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ dotenv.load();
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
       
     if (req.method === 'OPTIONS') {
